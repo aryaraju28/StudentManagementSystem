374b92df9d2b2f208de7ea1a79e07cd9a1f6e2ed
aryaraju28 <arya28199@gmail.com>
2021-03-19 20:36:09 +0530

    FinalCommit

--- StudentManagementSystem/Home.Designer.cs
+++ StudentManagementSystem/Home.Designer.cs
@@ -212,7 +212,7 @@ namespace StudentManagementSystem
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
-            this.ClientSize = new System.Drawing.Size(653, 358);
+            this.ClientSize = new System.Drawing.Size(643, 342);
             this.Controls.Add(this.btnResetPassword);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.linkPassword);
